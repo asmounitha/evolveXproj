30d05f6670ac37d40a39f3d30574c113ad58bbe6
asmounitha <asmounita04@gmail.com>
2026-03-04 18:29:42 +0530

    add prog

---
+++ progD3.3.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int N, i, sum = 0;
+    printf("Enter the value of N: ");
+    scanf("%d", &N);
+    for (i = 1; i <= N; i++) {
+        sum += i; 
+    }
+    printf("The sum of the first %d natural numbers is: %d\n", N, sum);
+
+    return 0;
+}
