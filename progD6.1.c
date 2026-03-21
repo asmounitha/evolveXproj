4ce4708748a20a085ce0b535c9b0ff947e9fc142
asmounitha <asmounita04@gmail.com>
2026-03-21 09:35:09 +0530

    added

--- progD6.1.c
+++ progD6.1.c
@@ -1,1 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    int ratings[7];
+    int i, max;
+    printf("Enter food ratings for 7 days:\n");
+    for(i = 0; i < 7; i++) {
+        scanf("%d", &ratings[i]);
+    }
+    max = ratings[0];
+    for(i = 1; i < 7; i++) {
+        if(ratings[i] > max) {
+            max = ratings[i];
+        }
+    }
+    printf("The highest food rating is: %d\n", max);
+
+    return 0;
+}
+
+
 
